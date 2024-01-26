38f9457198499fc547959c0d56332215b8a288b8
Ashade Samson <ashadesamson99@gmail.com>
2024-01-26 13:27:33 +0100

    added the user schema

---
+++ controllers/user.controller.js
@@ -0,0 +1,59 @@
+const userModel = require('../models/user_model')
+
+function getAllUsers(req, res) {
+    userModel.find()
+        .then(users => {
+            res.send(users)
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err)
+        })
+}
+
+function getUserByID(req, res) {
+    const id = req.params.id
+    userModel.findById(id)
+        .then(user => {
+            res.status(200).send(user)
+        }).catch(err => {
+            console.log(err)
+            res.status(404).send(err)
+        })
+}
+
+function createUser(req, res) {
+    const user = req.body
+    book.lastUpdateAt = new Date() 
+    userModel.create(user)
+        .then(user => {
+            res.status(201).send(user)
+        }).catch(err => {
+            console.log(err)
+            res.status(500).send(err)
+        })
+}
+
+function updateUser(req, res) {
+    const id = req.params.id
+    const user = req.body
+    book.lastUpdateAt = new Date()
+    userModel.findByIdAndUpdate(id, user, { new: true })
+        .then(newUser => {
+            res.status(200).send(newBook)
+        }).catch(err => {
+            console.log(err)
+            res.status(500).send(err)
+        })
+}
+
+function deleteUserByID(req, res) {
+    const id = req.params.id
+    userModel.findByIdAndRemove(id)
+        .then(user => {
+            res.status(200).send(user)
+        }).catch(err => {
+            console.log(err)
+            res.status(500).send(err)
+        })
+}
---
+++ models/user_model.js
@@ -0,0 +1,40 @@
+const moogoose = require('mongoose');
+
+const Schema = moogoose.Schema;
+
+
+
+const User_Schema = new Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    Role: {
+        type: String,
+        enum: ['private_chef', 'individual', 'catering_business'],
+        required: true
+    },
+    createAt : {
+        type: Date,
+        default: Date.now
+    },
+    lastUpdateAt : {
+        type: Date,
+        default: Date.now
+    },
+});
+
+
+module.exports = moogoose.model('users', User_Schema); 
